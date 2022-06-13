c484bbd8979ab2b8acf3ddc359bde7198573cef3
ryanprtma <ryanapratama16@gmail.com>
2022-06-14 02:54:05 +0700

    update

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (data) => {
+  return (async () => {
+    let result = 0
+    const res1 = await promiseTheaterIXX()
+    const res2 = await promiseTheaterVGC()
+    let newArr = [...res1, ...res2]
+    newArr.forEach(element => {
+      if (data == 'marah' && element.hasil == 'marah') {
+        result++
+      } else if (data == 'tidak marah' && element.hasil == 'tidak marah') {
+        result++
+      }
+    });
+    return result
+  })()
+}
 
 module.exports = {
   promiseOutput,
